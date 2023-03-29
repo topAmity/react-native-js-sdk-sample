8247ba9559408bf4bdbab6bbf26051338ec65cb8
top350 <44634314+top350@users.noreply.github.com>
2023-03-29 18:37:23 +0700

    Update code

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AmityClient, { ConnectionStatus, ApiEndpoint,ChannelRepository,ChannelFilter } from "@amityco/js-sdk";
+import { View, Button,Text } from "react-native";
+let client;
+function AuthProvider() {
+  const apiKey = ""; // Add your api key here
 
-export default function App() {
+  function login() {
+    client = new AmityClient({
+      apiKey: apiKey,
+      apiEndpoint: ApiEndpoint.SG,
+    }); // modify your server region here e.g ApiEndpoint.EU
+    client.registerSession({
+      userId: "",
+      displayName: "",
+    }); // Add your own userId and displayName
+    client.on("connectionStatusChanged", ({ newValue }) => {
+      if (newValue === ConnectionStatus.Connected) {
+        console.log("connected to asc");
+
+      } else {
+        console.log(" not connected to asc");
+      }
+    });
+    console.log("client: ", client);
+  }
+
+  function logout(){
+    client.unregisterSession();
+  }
+  function queryChannel() {
+ 
+    const liveCollection = ChannelRepository.queryChannels({
+      filter: ChannelFilter.Member,
+      sortBy:'lastActivity'
+    });
+
+    liveCollection.on("dataUpdated", (models) => {
+      console.log("Channel models: ", models);
+    });
+  }
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Button title='login'onPress={login}/>
+      <Text>Trigger login first, before calling other functions</Text>
+      <Button title='logout'onPress={logout}/>
+      <Button title='query channel'onPress={queryChannel}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default AuthProvider;
